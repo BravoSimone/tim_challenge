b1aea2a5240da355551858af430d447654a1ec38
Simone Bravo <simonebravo96@gmail.com>
2024-04-04 20:45:06 +0200

    Use string for author and text for text in quote

--- db/migrate/20240404122221_create_quotes.rb
+++ db/migrate/20240404122221_create_quotes.rb
@@ -1,8 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration[7.1]
   def change
     create_table :quotes do |t|
-      t.string :text
-      t.text :author
+      t.text :text
+      t.string :author
       t.integer :category
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_04_122221) do
   create_table "quotes", force: :cascade do |t|
-    t.string "text"
-    t.text "author"
+    t.text "text"
+    t.string "author"
     t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
